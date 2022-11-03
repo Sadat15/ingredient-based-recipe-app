4c73a8d7c0c92a92cf376f200f0e1c91a1071817
jean <jean.bustinza@gmail.com>
2022-11-03 16:08:21 +0000

    database first try

---
+++ server/models/ingredient.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const IngredientSchema = new mongoose.Schema({
+  idIngredient: Number,
+  strIngredient: String,
+  strDescription: String,
+  strType: String,
+  strAlcohol: String,
+  strABV: Number
+  
+
+})
+
+const Ingredient = mongoose.model("Ingredient", IngredientSchema)
+
+module.exports = Ingredient;
