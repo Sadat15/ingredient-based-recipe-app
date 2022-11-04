b50a51bcc65d21e24761f3eab3a184e524479954
Yichao <yq2550@gmail.com>
2022-11-04 12:38:33 +0000

    make separate api calls and concat data

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -5,10 +5,20 @@ const RecipesController = {
   All: async (req, res) => {
     // access drinks parameter
     const { drinks } = req.params;
-    const url = `https://www.thecocktaildb.com/api/json/v2/${process.env.COCKTAIL_API}/filter.php?i=${drinks}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
+    // const url = `https://www.thecocktaildb.com/api/json/v2/${process.env.COCKTAIL_API}/filter.php?i=${drinks}`;
+    const drinkArray = drinks.split(",");
+    let data;
+    let response;
+    for (i = 0; i < drinkArray.length; i++) {
+      const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${drinkArray[i]}`;
+      response = await fetch(url);
+      if (i === 0) {
+        data = await response.json();
+      }
+      data.drinks = data.drinks.concat(response.json().drinks);
+    }
+    // data.drinks = [...new Set(data.drinks)];
+    console.log(data.drinks);
     res.json(data);
   },
 
@@ -19,7 +29,7 @@ const RecipesController = {
     const data = await response.json();
     console.log(data);
     res.json(data);
-  }
+  },
 };
 
 module.exports = RecipesController;
