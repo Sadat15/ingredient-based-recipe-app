7ec145cefc1c3a0f2f0b618549b0b8502d306ab7
postie89 <t.mannion89@gmail.com>
2022-11-06 20:21:09 +0000

    added suggestion to search bar with popularity

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,17 +1,29 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import SearchItemButton from "./SearchItemButton";
-import BackgroundImage from '../img/background-image.png' 
+import BackgroundImage from "../img/background-image.png";
 import RecipeCardGroup from "./recipe-card-group";
-
 
 function SearchBar() {
   // onClick gets all recipes
   const [searchWord, setSearch] = useState("");
   const [search, setSearchQuery] = useState([]);
   const [recipes, setRecipes] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const loadAllIngredients = async () => {
+      const response = await axios.get(
+        `http://localhost:9000/ingredient/returnAllIngredients`
+      );
+      setIngredients(response.data);
+    };
+    loadAllIngredients();
+    console.log(ingredients);
+  }, []);
 
   const getRecipes = async () => {
     if (search.length > 0) {
@@ -29,6 +41,23 @@ function SearchBar() {
     }
   };
 
+  // suggest ingredients based on popularity and what the user has already typed
+  const suggestIngredients = (text) => {
+    let matches = [];
+    if (text.length > 0) {
+      matches = ingredients.filter((ingredient) => {
+        const regex = new RegExp("^" + text, "gi");
+        return ingredient.strIngredient.match(regex);
+      });
+      // sort by popularity
+      matches.sort((a, b) => {
+        return b.ingredientPopularity - a.ingredientPopularity;
+      });
+    }
+    setSearch(text);
+    setSuggestions(matches.slice(0, 5));
+  };
+
   const removeSearchItem = (item) => {
     const newSearch = search.filter((word) => word !== item);
     setSearchQuery(newSearch);
@@ -44,22 +73,45 @@ function SearchBar() {
     }
   };
 
+  const addFromSuggestion = (item) => {
+    // add the search word to the search array if it's not there yet
+    if (!search.includes(item)) {
+      const updatedSearch = search.push(item);
+      setSearch(updatedSearch);
+    }
+  };
+
   return (
     <div className="search-elements">
-      <div className="search-bar d-flex justify-content-center pb-2 text-center search-background" style={{backgroundImage: `url(${BackgroundImage})`}}>
+      <div
+        className="search-bar d-flex justify-content-center pb-2 text-center search-background"
+        style={{ backgroundImage: `url(${BackgroundImage})` }}
+      >
         <div className="container">
-          <br /><br /><br />
+          <br />
+          <br />
+          <br />
           <div className="row">
             <div className="col-1"></div>
             <div className="col-8">
-              
               <input
                 className="input-field form-control"
                 type="text"
                 placeholder="Search for a recipe"
-                onChange={(e) => setSearch(e.target.value)}
+                onChange={(e) => suggestIngredients(e.target.value)}
               />
-
+              {suggestions.map((suggestion, i) => (
+                <button
+                  className="search-item-button add-button mx-2 btn btn-primary"
+                  style={{ backgroundColor: "#25aec9", borderColor: "#25aec9" }}
+                  onClick={() => {
+                    addFromSuggestion(suggestion.strIngredient);
+                  }}
+                  key={`suggestion${i}`}
+                >
+                  {suggestion.strIngredient}
+                </button>
+              ))}
               <div className="text-center">
                 {search?.map((item) => (
                   <div
@@ -75,16 +127,16 @@ function SearchBar() {
             <div className="col-1">
               <button
                 className="add-button mx-2 btn btn-primary"
-                style={{backgroundColor: '#20577b', borderColor: '#20577b'}}
+                style={{ backgroundColor: "#20577b", borderColor: "#20577b" }}
                 onClick={addSearchWord}
               >
                 Add
               </button>
             </div>
             <div className="col-1">
-              <button 
-                className="search-button btn btn-primary" 
-                style={{backgroundColor: '#20577b', borderColor: '#20577b'}}
+              <button
+                className="search-button btn btn-primary"
+                style={{ backgroundColor: "#20577b", borderColor: "#20577b" }}
                 onClick={getRecipes}
               >
                 Search
@@ -92,20 +144,20 @@ function SearchBar() {
             </div>
             <div className="col-1"></div>
           </div>
-          <br /><br /><br />
+          <br />
+          <br />
+          <br />
         </div>
       </div>
-      <div className="search-items d-flex justify-content-center">
-        
-      </div>
+      <div className="search-items d-flex justify-content-center"></div>
       <div className="album py-5 bg-light">
         <div className="container">
           <div className="row">
             {recipes.drinks?.map((recipe) => (
-                <div className="col-md-3" key={recipe.idDrink}>
-                  <RecipeCardGroup recipe={recipe} />
-                </div>
-              ))}
+              <div className="col-md-3" key={recipe.idDrink}>
+                <RecipeCardGroup recipe={recipe} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
