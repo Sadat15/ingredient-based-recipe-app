ce040d764fc219a891a2b5a3be66a140b43dfbcc
postie89 <t.mannion89@gmail.com>
2022-11-09 10:48:27 +0000

    added infinite scroll to recipe-card-group

--- client/src/components/recipe-card-group.js
+++ client/src/components/recipe-card-group.js
@@ -1,31 +1,61 @@
 import RecipeCard from "./recipe-card";
 import { useOutletContext } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 // import searchResult from './SearchResult';
 
 function RecipeCardGroup() {
   const { recipes, setRecipes } = useOutletContext();
   const { search, setSearchQuery } = useOutletContext();
+  const itemsPerPage = 20;
+  const [hasMore, setHasMore] = useState(true);
+  const [records, setrecords] = useState(itemsPerPage);
 
   useEffect(() => {
     setRecipes(recipes);
     setSearchQuery(search);
+    // fetchData(setItems);
   }, [recipes, setRecipes, search, setSearchQuery]);
 
-  const background = {
-    background: "#86b0b3",
+  const showItems = (posts) => {
+    var items = [];
+    if (recipes.length > 0) {
+      for (var i = 0; i < records; i++) {
+        if (recipes[i]) {
+          items.push(
+            <div className="col-9 col-md-6 col-lg-3" key={posts[i].idDrink}>
+              <RecipeCard recipe={posts[i]} search={search} />
+            </div>
+          );
+        }
+      }
+    }
+    return items;
+  };
+
+  const loadMore = () => {
+    if (records === recipes.length) {
+      console.log("no more items");
+      setHasMore(false);
+    } else {
+      setTimeout(() => {
+        console.log("loading more items");
+        setrecords(records + itemsPerPage);
+      }, 2000);
+    }
   };
 
   return (
-    <div className="album py-5 background-gradient">
+    <div className="album py-5 background-gradient" id="scrollableDiv">
       <div className="container">
-        <div className="row d-flex justify-content-center">
-          {recipes?.slice(0, 20).map((recipe) => (
-            <div className="col-9 col-md-6 col-lg-3" key={recipe.idDrink}>
-              <RecipeCard recipe={recipe} search={search} />
-            </div>
-          ))}
-        </div>
+        <InfiniteScroll
+          dataLength={records}
+          next={loadMore}
+          hasMore={hasMore}
+          loader={<h4>Loading...</h4>}
+        >
+          <div className="row">{showItems(recipes)}</div>
+        </InfiniteScroll>
       </div>
     </div>
   );
